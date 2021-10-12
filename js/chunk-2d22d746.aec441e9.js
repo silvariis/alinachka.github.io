eb3bfec3f7be23615dbd774aee55480ff84c14d5
silvariis <90752236+silvariis@users.noreply.github.com>
2021-10-12 15:36:40 +0700

    Файл для сайта

---
+++ js/chunk-2d22d746.aec441e9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var a=t("7a23"),c={class:"about"},u=Object(a["i"])("h1",null,"This is an about page",-1);function s(n,e){return Object(a["u"])(),Object(a["e"])("div",c,[u])}var i=t("d959"),o=t.n(i);const d={},r=o()(d,[["render",s]]);e["default"]=r}}]);
+//# sourceMappingURL=chunk-2d22d746.aec441e9.js.map
